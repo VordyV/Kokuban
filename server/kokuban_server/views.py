cd53a758013d74673bf4b3180130212242cf3ad6
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2026-08-08 15:52:04 +0300

    Implemented the first endpoint `sendadminevent`

--- server/kokuban_server/views.py
+++ server/kokuban_server/views.py
@@ -1,4 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends, HTTPException
+from .admin_event_type import AdminEventType
+from .recipient_param_type import RecipientParamType
+from loguru import logger
+from typing import Annotated
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 
+async def Token(request: Request):
+	core = request.app.state.core
+	token = request.headers.get("auth")
+	if not token: raise HTTPException(status_code=401, detail="Token not specified")
+	srv_name = await core.check_token(token)
+	if not srv_name: raise HTTPException(status_code=401, detail="Token is invalid")
+	return srv_name
+
+@router.get('/sendadminevent', status_code=202, description="Send adminEvent. The recipient will be shown a dialog with info about the sent event")
+async def on_send_admin_event(request: Request, server_name: Annotated[str, Depends(Token)], type: AdminEventType, param: RecipientParamType, text: str = "", recipient: str | None = None):
+	core = request.app.state.core
+	try:
+		await core.sendAdminEvent(type, recipient=recipient, param=param, text=text)
+	except Exception as e:
+		raise HTTPException(status_code=400, detail=str(e))
+	logger.info(f"Server {server_name} sent an adminEvent to recipient {recipient} via parameter {param} with text '{text}'")

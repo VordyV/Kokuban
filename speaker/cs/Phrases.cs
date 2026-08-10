88a4ed457d35dced8de608d8c02544c06c80f991
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2026-08-10 20:55:44 +0300

    Added new phrases for error messages

--- speaker/cs/Phrases.cs
+++ speaker/cs/Phrases.cs
@@ -30,7 +30,10 @@ public class Phrases
         {"dialog.title.lostconn", "lost connection"},
         {"dialog.btn.close", "Close"},
         {"dialog.txt1", "To close the window, press CTRL"},
-        {"reason.ban", "You are banned on this server"}
+        {"reason.ban", "You are banned on this server"},
+        {"msg.err.invarg.title", "Invalid argument"},
+        {"msg.err.invargaddr.txt", "The kbsaddr argument contains no value or the value is invalid. A value in IPv4 format (XXX.XXX.XXX.XXX) is allowed"},
+        {"msg.err.invarport.txt", "The kbsport argument contains no value or the value is invalid. A value from 1 to 65535 is allowed"}
     };
     
     private static Dictionary<string, string> Ru = new ()
@@ -38,6 +41,9 @@ public class Phrases
         {"dialog.title.lostconn", "подключение потеряно"},
         {"dialog.btn.close", "Закрыть"},
         {"dialog.txt1", "Закрыть окно можно нажав CTRL"},
-        {"reason.ban", "Вы заблокированы на этом сервере"}
+        {"reason.ban", "Вы заблокированы на этом сервере"},
+        {"msg.err.invarg.title", "Некорректный аргумент"},
+        {"msg.err.invargaddr.txt", "Аргумент kbsaddr не содержит значения или оно некорректное. Допустимо значение в формате IPv4 (XXX.XXX.XXX.XXX)"},
+        {"msg.err.invarport.txt", "Аргумент kbsport не содержит значения или оно некорректное. Допустимо значение от 1 до 65535"}
     };
 }

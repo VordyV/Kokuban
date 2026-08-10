cdaa0542839fc14846834391005075df6cfbd4fc
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2026-08-10 20:56:32 +0300

    Added readonly modifiers for TITLE and VERSION

--- speaker/cs/Program.cs
+++ speaker/cs/Program.cs
@@ -2,9 +2,9 @@
 
 public class Program
 {
-    public static string TITLE = "KBS";
+    public static readonly string TITLE = "KBS";
     public static string ADDRESS = "127.0.0.1";
     public static ushort PORT = 8080;
     public static string LANG = "ru";
-    public static string VERSION = "1.0";
+    public static readonly string VERSION = "1.0";
 }

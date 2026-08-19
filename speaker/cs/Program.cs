7801fdbb3f5dcbd24c65e90aa393ae9a66e2be5e
Vordy <senior.dkflgjyjvfhdsetev@yandex.ru>
2026-08-19 22:02:34 +0300

    Updated the version to 2.0

--- speaker/cs/Program.cs
+++ speaker/cs/Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static string ADDRESS = "127.0.0.1";
     public static ushort PORT = 8080;
     public static string LANG = "ru";
-    public static readonly string VERSION = "1.0";
+    public static readonly string VERSION = "2.0";
     public static readonly TimeSpan CENTRAL_TEXT_PERIOD = TimeSpan.FromSeconds(7);
     public static readonly TimeSpan BOTTOM_STATUS_BAR_PERIOD = TimeSpan.FromSeconds(3);
 }
